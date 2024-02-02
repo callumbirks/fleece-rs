e862fc26c8bed66a661ab56eb3e3cd53d3393d3a
Callum Birks <callumbirks@protonmail.com>
2024-02-02 01:15:15 +0000

    Rewrite Encoder. impl Encodable. SharedKeys. Some misc changes

--- src/encoder/encodable.rs
+++ src/encoder/encodable.rs
@@ -1,165 +1,137 @@
 use crate::encoder::{Encodable, NullValue, UndefinedValue};
 use crate::raw::{value, varint};
-use std::io::Write;
-
-fn write_fleece_int<W: Write, const UNSIGNED: bool>(writer: &mut W, value: i64) -> Option<()> {
-    let mut buf = [0_u8; 9];
-    //#[allow(clippy::cast_possible_truncation)]
-    //let trailing_zeros = if UNSIGNED || value >= 0 {
-    //    buf.iter().rev().take_while(|b| **b == 0).count() as u8
-    //} else {
-    //    // Signed integers are sign extended, so we need to count the trailing 1s
-    //    buf.iter().rev().take_while(|b| **b == 0xFF).count() as u8
-    //};
-    //let size = 8 - trailing_zeros;
-    let byte_count = value.fleece_size() - 1;
-    buf[0] = value::tag::INT | ((byte_count as u8) - 1);
-    buf[1..].copy_from_slice(&value.to_le_bytes());
-    writer.write_all(&buf[..=(byte_count as usize)]).ok()
-}
 
 impl Encodable for i64 {
-    fn write_fleece_to<W: Write>(&self, writer: &mut W) -> Option<()> {
+    fn write_fleece_to(&self, bytes: &mut [u8]) -> Option<()> {
         // If this is small enough, store it as a short
         if *self <= 2047 || *self >= -2048 {
             #[allow(clippy::cast_possible_truncation)]
-            return (*self as i16).write_fleece_to(writer);
+            return (*self as i16).write_fleece_to(bytes);
         }
-        return write_fleece_int::<_, false>(writer, *self);
-    }
-
-    fn fleece_size(&self) -> usize {
-        if *self <= 2047 || *self >= -2048 {
-            return 2;
+        let mut buf = [0_u8; 9];
+        buf[1..].copy_from_slice(&self.to_le_bytes());
+        #[allow(clippy::cast_possible_truncation)]
+        let trailing_zeros = buf.iter().rev().take_while(|b| **b == 0).count() as u8;
+        let size = 8 - trailing_zeros;
+        if bytes.len() < size as usize + 1 {
+            return None;
         }
-        return if *self >= 0 {
-            8 - self.trailing_zeros() + 1
-        } else {
-            8 - self.trailing_ones() + 1
-        } as usize
+        buf[0] = value::tag::INT | (size - 1);
+        bytes[..=(size as usize)].copy_from_slice(&buf[..=(size as usize)]);
+        Some(())
     }
 }
 
 impl Encodable for u64 {
-    fn write_fleece_to<W: Write>(&self, writer: &mut W) -> Option<()> {
+    fn write_fleece_to(&self, bytes: &mut [u8]) -> Option<()> {
         // If this is small enough, store it as a short
         if *self <= 2047 {
             #[allow(clippy::cast_possible_truncation)]
-            return (*self as u16).write_fleece_to(writer);
+            return (*self as u16).write_fleece_to(bytes);
         }
-        return write_fleece_int::<_, true>(writer, *self as i64);
-    }
-
-    fn fleece_size(&self) -> usize {
-        if *self <= 2047 {
-            2
-        } else {
-            8 - self.trailing_zeros() as usize + 1
+        let mut buf = [0_u8; 9];
+        buf[1..].copy_from_slice(&self.to_le_bytes());
+        #[allow(clippy::cast_possible_truncation)]
+        let trailing_zeros = buf.iter().rev().take_while(|b| **b == 0).count() as u8;
+        let size = 8 - trailing_zeros;
+        if bytes.len() < size as usize + 1 {
+            return None;
         }
+        buf[0] = value::tag::INT | 0x08 | ((size - 1) & 0x07);
+        bytes[..=(size as usize)].copy_from_slice(&buf[..=(size as usize)]);
+        Some(())
     }
 }
 
 impl Encodable for u16 {
-    fn write_fleece_to<W: Write>(&self, writer: &mut W) -> Option<()> {
+    fn write_fleece_to(&self, bytes: &mut [u8]) -> Option<()> {
+        if bytes.len() < 2 {
+            return None;
+        }
         // Short can only be 12 bits
         if *self > 2047 {
-            return u64::from(*self).write_fleece_to(writer);
+            return u64::from(*self).write_fleece_to(bytes);
         }
-        let mut buf: [u8; 2] = self.to_be_bytes();
-        buf[0] |= value::tag::SHORT;
-        writer.write_all(&buf).ok()
-    }
-
-    fn fleece_size(&self) -> usize {
-        2
+        bytes[0..2].copy_from_slice(&self.to_be_bytes());
+        bytes[0] |= value::tag::SHORT;
+        Some(())
     }
 }
 
 impl Encodable for i16 {
-    fn write_fleece_to<W: Write>(&self, writer: &mut W) -> Option<()> {
+    fn write_fleece_to(&self, bytes: &mut [u8]) -> Option<()> {
+        if bytes.len() < 2 {
+            return None;
+        }
         // Short can only be 12 bits
         if *self > 2047 || *self < -2048 {
-            return i64::from(*self).write_fleece_to(writer);
+            return i64::from(*self).write_fleece_to(bytes);
         }
-        let mut buf: [u8; 2] = self.to_be_bytes();
-        // Make sure to zero out the top 4 bits (where the tag goes) in-case of sign extension
-        buf[0] = (buf[0] & 0x0F) | value::tag::SHORT;
-        writer.write_all(&buf).ok()
-    }
-
-    fn fleece_size(&self) -> usize {
-        2
+        bytes[0..2].copy_from_slice(&self.to_be_bytes());
+        bytes[0] = (bytes[0] & 0x0F) | value::tag::SHORT;
+        Some(())
     }
 }
 
 impl Encodable for f32 {
-    fn write_fleece_to<W: Write>(&self, writer: &mut W) -> Option<()> {
-        let mut buf = [0_u8; 6];
-        buf[0] = value::tag::FLOAT;
-        buf[2..6].copy_from_slice(&self.to_le_bytes());
-        writer.write_all(&buf).ok()
-    }
-
-    fn fleece_size(&self) -> usize {
-        6
+    fn write_fleece_to(&self, bytes: &mut [u8]) -> Option<()> {
+        if bytes.len() < 6 {
+            return None;
+        }
+        bytes[0] = value::tag::FLOAT;
+        bytes[2..6].copy_from_slice(&self.to_le_bytes());
+        Some(())
     }
 }
 
 impl Encodable for f64 {
-    fn write_fleece_to<W: Write>(&self, writer: &mut W) -> Option<()> {
-        let mut buf = [0_u8; 10];
-        buf[0] = value::tag::FLOAT | 0x08;
-        buf[2..10].copy_from_slice(&self.to_le_bytes());
-        writer.write_all(&buf).ok()
-    }
-
-    fn fleece_size(&self) -> usize {
-        10
+    fn write_fleece_to(&self, bytes: &mut [u8]) -> Option<()> {
+        if bytes.len() < 10 {
+            return None;
+        }
+        bytes[0] = value::tag::FLOAT | 0x08;
+        bytes[2..10].copy_from_slice(&self.to_le_bytes());
+        Some(())
     }
 }
 
-fn write_fleece_constant<W: Write>(writer: &mut W, constant: [u8; 2]) -> Option<()> {
-    writer.write_all(&constant).ok()
+fn write_fleece_constant(bytes: &mut [u8], constant: [u8; 2]) -> Option<()> {
+    if bytes.len() < 2 {
+        return None;
+    }
+    bytes[0..2].copy_from_slice(&constant);
+    Some(())
 }
 
 impl Encodable for bool {
-    fn write_fleece_to<W: Write>(&self, writer: &mut W) -> Option<()> {
+    fn write_fleece_to(&self, bytes: &mut [u8]) -> Option<()> {
         if *self {
-            write_fleece_constant(writer, value::constants::TRUE)
+            write_fleece_constant(bytes, value::constants::TRUE)
         } else {
-            write_fleece_constant(writer, value::constants::FALSE)
+            write_fleece_constant(bytes, value::constants::FALSE)
         }
-    }
-
-    fn fleece_size(&self) -> usize {
-        2
     }
 }
 
 impl Encodable for NullValue {
-    fn write_fleece_to<W: Write>(&self, writer: &mut W) -> Option<()> {
-        write_fleece_constant(writer, value::constants::NULL)
-    }
-
-    fn fleece_size(&self) -> usize {
-        2
+    fn write_fleece_to(&self, bytes: &mut [u8]) -> Option<()> {
+        write_fleece_constant(bytes, value::constants::NULL)
     }
 }
 
 impl Encodable for UndefinedValue {
-    fn write_fleece_to<W: Write>(&self, writer: &mut W) -> Option<()> {
-        write_fleece_constant(writer, value::constants::UNDEFINED)
-    }
-
-    fn fleece_size(&self) -> usize {
-        2
+    fn write_fleece_to(&self, bytes: &mut [u8]) -> Option<()> {
+        write_fleece_constant(bytes, value::constants::UNDEFINED)
     }
 }
 
 // Data and String are encoded the same in Fleece, apart from the value type tag.
-fn write_fleece_string<W: Write, const IS_STR: bool>(string: &[u8], writer: &mut W) -> Option<()> {
-    let mut buf = [0_u8; 2];
-    buf[0] = if IS_STR {
+fn write_fleece_string<const IS_STR: bool>(string: &[u8], bytes: &mut [u8]) -> Option<()> {
+    if bytes.len() < 2 {
+        return None;
+    }
+
+    bytes[0] = if IS_STR {
         value::tag::STRING
     } else {
         value::tag::DATA
@@ -167,55 +139,45 @@ fn write_fleece_string<W: Write, const IS_STR: bool>(string: &[u8], writer: &mut
 
     match string.len() {
         // If size is 1 or 0, size fits in the tiny value and string fits in the second byte.
-        0 => writer.write_all(&buf[..2]).ok(),
+        0 => {}
         1 => {
-            buf[0] |= 1;
-            buf[1] = string[0];
-            writer.write_all(&buf[..2]).ok()
+            bytes[0] |= 1;
+            bytes[1] = string[0];
         }
         // If size is up to 0x0E (0x0F is the bit that indicates a varint), we can fit the size in the tiny value.
         #[allow(clippy::cast_possible_truncation)]
         2..=0x0E => {
-            buf[0] |= string.len() as u8;
-            writer.write_all(&buf[0..1]).ok()?;
-            writer.write_all(string).ok()
+            if bytes.len() < 1 + string.len() {
+                return None;
+            }
+            bytes[0] |= string.len() as u8;
+            bytes[1..=string.len()].copy_from_slice(string);
         }
         // Any larger sizes will store the size as a varint.
         _ => {
-            buf[0] |= 0x0F;
+            bytes[0] |= 0x0F;
             let mut varint_buf = [0_u8; varint::MAX_LEN];
             let varint_size = varint::write(&mut varint_buf, string.len() as u64);
-            // Write the tag + tiny (1 byte)
-            writer.write_all(&buf[0..1]).ok()?;
-            // Write the varint
-            writer.write_all(&varint_buf[..varint_size]).ok()?;
-            // Write the string
-            writer.write_all(string).ok()
+            let total_size = 1 + varint_size + string.len();
+            if bytes.len() < total_size {
+                return None;
+            }
+            bytes[1..=varint_size].copy_from_slice(&varint_buf[..varint_size]);
+            bytes[varint_size + 1..total_size].copy_from_slice(string);
         }
+    }
+    Some(())
+}
+
+impl Encodable for str {
+    fn write_fleece_to(&self, bytes: &mut [u8]) -> Option<()> {
+        write_fleece_string::<true>(self.as_bytes(), bytes)
     }
 }
 
 impl Encodable for [u8] {
-    fn write_fleece_to<W: Write>(&self, writer: &mut W) -> Option<()> {
-        write_fleece_string::<_, false>(self, writer)
-    }
-
-    fn fleece_size(&self) -> usize {
-        match self.len() {
-            0 | 1 => 2,
-            2..=0x0E => 1 + self.len(),
-            _ => 1 + varint::size_required(self.len()) + self.len()
-        }
-    }
-}
-
-impl Encodable for str {
-    fn write_fleece_to<W: Write>(&self, writer: &mut W) -> Option<()> {
-        write_fleece_string::<_, true>(self.as_bytes(), writer)
-    }
-
-    fn fleece_size(&self) -> usize {
-        self.as_bytes().fleece_size()
+    fn write_fleece_to(&self, bytes: &mut [u8]) -> Option<()> {
+        write_fleece_string::<false>(self, bytes)
     }
 }
 
@@ -223,17 +185,10 @@ impl<T> Encodable for Option<T>
 where
     T: Encodable,
 {
-    fn write_fleece_to<W: Write>(&self, writer: &mut W) -> Option<()> {
+    fn write_fleece_to(&self, bytes: &mut [u8]) -> Option<()> {
         match self {
-            Some(value) => value.write_fleece_to(writer),
-            None => NullValue.write_fleece_to(writer),
-        }
-    }
-
-    fn fleece_size(&self) -> usize {
-        match self {
-            Some(value) => value.fleece_size(),
-            None => 2
+            Some(value) => value.write_fleece_to(bytes),
+            None => NullValue.write_fleece_to(bytes),
         }
     }
 }
--- src/encoder/mod.rs
+++ src/encoder/mod.rs
@@ -1,91 +1,41 @@
 use crate::sharedkeys::SharedKeys;
 use std::borrow::Borrow;
-use std::cell::RefCell;
-use std::io::Write;
 use std::rc::Rc;
 
 mod encodable;
-mod value_stack;
 
 struct NullValue;
 struct UndefinedValue;
 
 // Implementations are in the `encodable` module
 pub trait Encodable {
-    fn write_fleece_to<W: Write>(&self, writer: &mut W) -> Option<()>;
-    fn fleece_size(&self) -> usize;
-}
-
-enum LastAdded {
-    Key,
-    Value,
+    fn write_fleece_to(&self, bytes: &mut [u8]) -> Option<()>;
 }
 
 pub struct Encoder {
-    out: Vec<u8>,
-    shared_keys: Option<Rc<RefCell<SharedKeys>>>,
-    // value_stack: [sized::NarrowValue],
-    // array_stack: ArrayStack,
-    value_stack: value_stack::ValueStack,
-    last_added: LastAdded,
+    bytes: Vec<u8>,
+    shared_keys: Rc<SharedKeys>,
 }
 
 impl Encoder {
     pub fn new() -> Self {
         Self {
-            out: Vec::new(),
-            shared_keys: None,
-            value_stack: value_stack::ValueStack::new(),
-            last_added: LastAdded::Value,
-        }
-    }
-
-    pub fn new_with_shared_keys(shared_keys: Rc<RefCell<SharedKeys>>) -> Self {
-        Self {
-            out: Vec::new(),
-            shared_keys: Some(shared_keys),
-            value_stack: value_stack::ValueStack::new(),
-            last_added: LastAdded::Value,
+            bytes: Vec::new(),
+            shared_keys: Rc::new(SharedKeys::new()),
         }
     }
 
     pub fn write_key(&mut self, key: &str) -> Option<()> {
-        if matches!(self.last_added, LastAdded::Key) {
-            return None;
-        }
-        let result = if let Some(shared_keys) = &self.shared_keys {
-            let int_key = shared_keys.borrow_mut().encode_and_insert(key)?;
-            int_key.write_fleece_to(&mut self.out)
-        } else {
-            key.write_fleece_to(&mut self.out)
-        };
-        if result.is_some() {
-            self.last_added = LastAdded::Key;
-        }
-        result
+        key.write_fleece_to(&mut self.bytes)
     }
 
-    /// Write an [`Encodable`] type to the encoder. The parameter may be any borrowed form of an Encodable type.
+    /// Write an encodable type to the encoder. The parameter may be any borrowed form of an encodable type.
     pub fn write<R, T>(&mut self, value: &R) -> Option<()>
     where
-        R: Borrow<T> + ?Sized,
+        R: Borrow<T>,
         T: Encodable + ?Sized,
     {
-        if matches!(self.last_added, LastAdded::Value) {
-            return None;
-        }
-        let value = value.borrow();
-        let result: Option<()> = if value.fleece_size() > 4 {
-            // Pointer, add value to output and pointer to stack
-            todo!()
-        } else {
-            // Inline, add to stack
-            todo!()
-        };
-        if result.is_some() {
-            self.last_added = LastAdded::Value;
-        }
-        result
+        value.borrow().write_fleece_to(&mut self.bytes)
     }
 
     pub fn begin_array(&mut self, count: usize) {
@@ -104,13 +54,7 @@ impl Encoder {
         todo!()
     }
 
-    pub fn finish(mut self) -> Box<[u8]> {
-        // Shrink because `into_boxed_slice` will allocate a new buffer if the capacity is greater than the length
-        self.out.shrink_to_fit();
-        self.out.into_boxed_slice()
-    }
-
-    pub fn set_shared_keys(&mut self, shared_keys: Rc<RefCell<SharedKeys>>) {
-        self.shared_keys = Some(shared_keys);
+    pub fn finish(self) -> Box<[u8]> {
+        self.bytes.into_boxed_slice()
     }
 }
